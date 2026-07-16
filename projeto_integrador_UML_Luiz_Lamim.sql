2acf8fd1e69e06d645b6a34562ecb388be3e5ebc
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-16 00:04:32 -0300

    Criação das Tabelas Vinculadas ao Cliente.

--- projeto_integrador_UML_Luiz_Lamim.sql
+++ projeto_integrador_UML_Luiz_Lamim.sql
@@ -15,3 +15,45 @@ CREATE TABLE Funcionario (
     Nome_Funcionario VARCHAR(100) NOT NULL,
     PRIMARY KEY (ID_Funcionario)
 );
+
+-- Tabela Telefone
+CREATE TABLE Telefone (
+    ID_Telefone INT AUTO_INCREMENT,
+    CPF_Cliente CHAR(14) NOT NULL,
+    Numero VARCHAR(20) NOT NULL,
+    Tipo VARCHAR(20),
+    PRIMARY KEY (ID_Telefone),
+    FOREIGN KEY (CPF_Cliente) REFERENCES Cliente (CPF)
+);
+
+-- Tabela Email
+CREATE TABLE Email (
+    ID_Email INT AUTO_INCREMENT,
+    CPF_Cliente CHAR(14) NOT NULL,
+    Endereco_Email VARCHAR(100) NOT NULL,
+    PRIMARY KEY (ID_Email),
+    FOREIGN KEY (CPF_Cliente) REFERENCES Cliente(CPF)
+);
+
+-- Tabela Conta Bancaria
+CREATE TABLE Conta_Bancaria (
+    ID_Conta INT AUTO_INCREMENT,
+    CPF_Cliente CHAR(14) NOT NULL,
+    Banco VARCHAR(50) NOT NULL,
+    Agencia VARCHAR(10) NOT NULL,
+    Conta VARCHAR(20) NOT NULL,
+    Saldo DECIMAL(15,2),
+    Data_Saldo DATE,
+    PRIMARY KEY (ID_Conta),
+    FOREIGN KEY (CPF_Cliente) REFERENCES Cliente (CPF)
+);
+
+-- Tabela Investimento
+CREATE TABLE Investimento (
+    ID_Investimento INT AUTO_INCREMENT,
+    CPF_Cliente CHAR(14) NOT NULL,
+    Tipo_Investimento VARCHAR(50) NOT NULL,
+    Valor_Investido DECIMAL(15,2),
+    PRIMARY KEY (ID_Investimento),
+    FOREIGN KEY (CPF_Cliente) REFERENCES Cliente (CPF)
+);
