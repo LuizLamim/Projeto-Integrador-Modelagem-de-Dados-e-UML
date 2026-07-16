5ed439b48763f90071edd0d4f55161bace027d22
LuizLamim <luiz.britohenrique@hotmail.com>
2026-07-16 00:04:00 -0300

    Criação das Tabelas Principais.

--- projeto_integrador_UML_Luiz_Lamim.sql
+++ projeto_integrador_UML_Luiz_Lamim.sql
@@ -1,3 +1,17 @@
 -- Criação do banco de dados
 CREATE DATABASE XPTO_Investimentos;
 USE XPTO_Investimentos;
+
+-- Tabela Cliente
+CREATE TABLE Cliente (
+    CPF CHAR(14) NOT NULL,
+    Nome VARCHAR(100) NOT NULL,
+    PRIMARY KEY (CPF)
+);
+
+-- Tabela Funcionario
+CREATE TABLE Funcionario (
+    ID_Funcionario INT AUTO_INCREMENT,
+    Nome_Funcionario VARCHAR(100) NOT NULL,
+    PRIMARY KEY (ID_Funcionario)
+);
